fdcf94478fb78fa4819095dc99cb059f8d45fc06
Vinit Kanpariya <vinit.k@solutlabs.com>
2025-02-05 14:52:10 +0530

    next.config.js update

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,15 @@
 module.exports = {
-    async headers() {
-      return [
-        {
-          source: '/posts/:id*',
-          headers: [
-            {
-              key: 'Cache-Control',
-              value: 'public, max-age=120, stale-while-revalidate=60',
-            },
-          ],
-        },
-      ];
-    },
-  };
-  
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=120, stale-while-revalidate=60',
+          },
+        ],
+      },
+    ];
+  },
+};
